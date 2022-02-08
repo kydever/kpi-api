70cb82f9b147aecbb4120e7c6423aa9434943341
laradocs <75692074@qq.com>
2022-02-08 22:24:55 +0800

    代码格式化

--- app/Model/Department.php
+++ app/Model/Department.php
@@ -1,21 +1,29 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace App\Model;
 
 /**
- * @property int $id 
- * @property string $chat_id 
- * @property string $department_id 
- * @property string $leader_user_id 
- * @property int $member_count 
- * @property string $name 
- * @property string $open_department_id 
- * @property int $order 
- * @property int $parent_department_id 
- * @property string $status 
- * @property \Carbon\Carbon $created_at 
- * @property \Carbon\Carbon $updated_at 
+ * @property int $id
+ * @property string $chat_id
+ * @property string $department_id
+ * @property string $leader_user_id
+ * @property int $member_count
+ * @property string $name
+ * @property string $open_department_id
+ * @property int $order
+ * @property int $parent_department_id
+ * @property string $status
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  */
 class Department extends Model
 {
@@ -23,10 +31,12 @@ class Department extends Model
      * The table associated with the model.
      */
     protected ?string $table = 'departments';
+
     /**
      * The attributes that are mass assignable.
      */
     protected array $fillable = ['id', 'chat_id', 'department_id', 'leader_user_id', 'member_count', 'name', 'open_department_id', 'order', 'parent_department_id', 'status', 'created_at', 'updated_at'];
+
     /**
      * The attributes that should be cast to native types.
      */
