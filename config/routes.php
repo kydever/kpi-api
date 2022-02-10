01e52b32df8ee2b890c563df8352a21c31496cdf
laradocs <75692074@qq.com>
2022-02-10 13:07:51 +0800

    路由

--- config/routes.php
+++ config/routes.php
@@ -13,9 +13,6 @@ use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController::index');
 
-Router::post('/works', [App\Controller\WorkController::class, 'store']);
-Router::patch('/works/{id:\d+}', [App\Controller\WorkController::class, 'update']);
-Router::delete('/works/{id:\d+}', [App\Controller\WorkController::class, 'destroy']);
 Router::post('/classifies', [App\Controller\ClassifyController::class, 'store']);
 Router::patch('/classifies/{id:\d+}', [App\Controller\ClassifyController::class, 'update']);
 Router::delete('/classifies/{id:\d+}', [App\Controller\ClassifyController::class, 'destroy']);
